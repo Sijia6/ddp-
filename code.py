bb5376c062f38c330a9f5ab09b747c44a8497be1
Sijia6 <156685319+Sijia6@users.noreply.github.com>
2024-01-15 14:46:47 +0000

    Add files via upload

---
+++ code.py
@@ -0,0 +1,17 @@
+def add(num1,num2):
+    return num1 + num2
+print(add (7,10))
+
+def subtract(num1,num2):
+    return num1-num2
+print(subtract(10,7))
+
+def multiply(num1,num2):
+    return num1*num2
+print(multiply(2,3))
+
+def divide(num1,num2):
+    return num1/num2
+print(divide(2,3))
+print(divide(3,4))
+print(divide(2,5))
